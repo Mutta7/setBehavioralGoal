22b1922668b4aa2f1a1d236588b1c64df4901660
Nozomu Hasegawa <hsnznewton7@gmail.com>
2022-04-25 21:32:53 +0900

    implement sample code for connecting and get data from postgreSQL db

--- sql/test_connect_psql.js
+++ sql/test_connect_psql.js
@@ -1,1 +1,31 @@
-// write test script
+const { Client } = require("pg");
+const fs = require("fs");
+
+const postgre_credential_text = fs.readFileSync("./secret/postgresql.json");
+const postgre_credential_json = JSON.parse(postgre_credential_text);
+
+const client = new Client({
+    user: postgre_credential_json.username,
+    host: "127.0.0.1",
+    database: "nodedb_for_behavioralgoal",
+    password: postgre_credential_json.password,
+    port: 5432,
+})
+client.connect();
+/*
+var insertQuery = {
+    text: "INSERT INTO member_dev VALUES ($1, $2)",
+    values: [1, "John Smith"],
+};
+*/
+
+var selectQuery = {
+    text: "SELECT * FROM member_dev",
+}
+
+client.query(selectQuery)
+    .then((res) => {
+        console.log(res);
+        client.end();
+    })
+    .catch( (e) => console.error(e.stack));
