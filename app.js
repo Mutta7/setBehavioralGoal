6d6ead003efe28a53daaf0f87196435dceee1441
Nozomu Hasegawa <hsnznewton7@gmail.com>
2022-05-07 19:02:04 +0900

    add put api.

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const apiSampleRouter = require("./routes/api_actionGoals");
 app.set("views", `${__dirname}/views`);
 app.set("view engine", "ejs");
 
+app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use("/api_actionGoals", apiSampleRouter);
--- routes/api_actionGoals.js
+++ routes/api_actionGoals.js
@@ -14,4 +14,44 @@ router.get("/", function (req, res, next) {
     });
   });
   
-  module.exports = router
+router.get("/:id", function(req, res, next) {
+    pool.query("SELECT * FROM action_goals_dev2 where id = $1", [id], function (error, result) {
+        if (error) {
+          throw error;
+        }
+        res.status(200).json({
+          data: result.rows,
+        });
+      });
+});
+
+router.put("/:id", function(req, res, next) {
+    const id = req.params.id;
+    console.log(req.body);
+    const {status} = req.body.put_data;
+    console.log(status);
+    pool.query(
+        "UPDATE action_goals_dev2 SET status = $1 WHERE id = $2",
+        [status, id],
+        function(error, results) {
+            if (error) {
+                res.status(500).json({
+                    status : "500 Inernal Server Error",
+                    error: error,
+                });
+            }
+            if (results.rowCount === 0) {
+                res.status(400).json({
+                    status: "400 Bad Request",
+                    message: "data is not exist.",
+                });
+            } else {
+                res.status(200).json({
+                    status: "success",
+                });
+            }
+        }
+    );
+});
+
+module.exports = router
